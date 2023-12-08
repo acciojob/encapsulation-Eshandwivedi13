d339a81b768a77e4703f7f0eed324bafe60d386c
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2023-12-08 19:53:04 +0530

    FIRST

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[]){
+        RWOnly obj = new RWOnly();
+//        obj.name = "ABC";
+//        System.out.println(obj.name);//name has private access in com.driver.RWOnly
+        obj.setter("ABC");
+        System.out.println(obj.getter());
+    }
 }
